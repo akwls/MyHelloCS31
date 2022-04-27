a9d2acd7cd262d1925f6baca1f11f148df1f6655
akwls <rhrnak624@naver.com>
2022-04-27 10:12:36 +0900

    #2 그럴싸한 환영 인사 추가 환영 인사 메세지가 은근 마음에 든다...

--- MyHelloCS31/Form1.cs
+++ MyHelloCS31/Form1.cs
@@ -19,7 +19,7 @@ namespace MyHelloCS31
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("어서오시게나", "주인장 말씀", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
         }
     }
 }
